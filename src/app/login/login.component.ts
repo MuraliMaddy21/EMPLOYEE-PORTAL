edeb9a7c9020e901a8958018fc40c64069144ff7
MuraliMaddy21 <110399320+MuraliMaddy21@users.noreply.github.com>
2023-03-18 10:32:16 +0530

    error handling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Route,Router } from '@angular/router';
 import { HttpClient} from '@angular/common/http';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -57,7 +58,13 @@ export class LoginComponent implements OnInit {
     "password":this.password
    }
    console.log(this.json)
-   this.http.post('http://localhost:3030/eplogin',this.json,{responseType:'json'}).subscribe((response=>
+   this.http.post('http://localhost:3030/eplogin',this.json,{responseType:'json'}).pipe(
+    catchError(error => {
+      console.error(error);
+      window.alert("An error occurred while processing your request.Please ensure that the backend is running or Please try again later.");
+      return []; // return an empty array to continue the observable stream
+    })
+  ).subscribe((response=>
    {
       this.result = response
       console.log(this.result)
@@ -67,6 +74,10 @@ export class LoginComponent implements OnInit {
       {
          window.alert("Login Successful")
         this.route.navigate(["/profile"]);
+      }
+      else if(this.gotp!=this.lotp)
+      {
+        window.alert("Incorrect OTP!Kindly recheck the opt and enter the correct otp")
       }
       else
       {
@@ -104,7 +115,13 @@ export class LoginComponent implements OnInit {
     {
       "otp":this.lotp
     }
-    this.http.post('http://localhost:3030/otp',this.json2,{responseType:'json'}).subscribe((response)=>
+    this.http.post('http://localhost:3030/otp',this.json2,{responseType:'json'}).pipe(
+      catchError(error => {
+        console.error(error);
+        window.alert("An error occurred while processing your request.Please ensure that the backend is running or Please try again later.");
+        return []; // return an empty array to continue the observable stream
+      })
+    ).subscribe((response)=>
    {
       this.result = response
       console.log(this.result)
